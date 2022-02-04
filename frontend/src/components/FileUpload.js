5f40c2fae4693bbdec9a188d67badbb680604a91
Lupan Vasile <66137225+Vascan5123@users.noreply.github.com>
2022-02-04 10:21:32 +0200

    add styles [part1]

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -5,68 +5,72 @@ import { getDroppedOrSelectedFiles } from "html5-file-selector";
 
 
 const FileUpload = (props) => {
-  const fileParams = ({ file, meta }) => {
-    const body = new FormData();
-    body.append("profile_pic", file);
-    console.log(file);
-    return { url: "http://127.0.0.1:8000/get_file/", body }; // localhost
-  };
+    const fileParams = ({ file, meta }) => {
+        const body = new FormData();
+        body.append("profile_pic", file);
+        console.log(file);
+        return { url: "https://httpbin.org/post", body }; // localhost
+        //http://127.0.0.1:8000/get_file/
+        //https://httpbin.org/post
+    };
 
-  const onFileChange = ({ meta, file }, status) => {
-    console.log(file, meta, status);
-  };
+    const onFileChange = ({ meta, file }, status) => {
+        console.log(file, meta, status);
+    };
 
-  const handleSubmit = (files) => { 
-    props.updateStore({sourceFiles: ["saasfasf"]}); 
-    // files.map(f => f.meta)
-  }
-  
+    const handleSubmit = (files) => {
+        props.updateStore({ sourceFiles: ["saasfasf"] });
+        // files.map(f => f.meta)
+    }
 
-  const getFilesFromEvent = (e) => {
-    return new Promise((resolve) => {
-      getDroppedOrSelectedFiles(e).then((chosenFiles) => {
-        resolve(chosenFiles.map((f) => f.fileObject));
-      });
-    });
-  };
 
-  const selectFileInput = ({ accept, onFiles, files, getFilesFromEvent }) => {
-    const textMsg =
-      files.length > 0 ? "Mai selectează un fișier" : "Selectează fișierele din calculatorul tău";
-
-    return (
-      <label className="btn btn-danger mt-4">
-        {textMsg}
-        <input
-          style={{ display: "none" }}
-          type="file"
-          accept={accept}
-          multiple
-          onChange={(e) => {
-            getFilesFromEvent(e).then((chosenFiles) => {
-              onFiles(chosenFiles);
+    const getFilesFromEvent = (e) => {
+        return new Promise((resolve) => {
+            getDroppedOrSelectedFiles(e).then((chosenFiles) => {
+                resolve(chosenFiles.map((f) => f.fileObject));
             });
-          }}
-        />
-      </label>
-    );
-  };
+        });
+    };
 
-  return (
-    <Dropzone
-      onSubmit={handleSubmit}
-      onChangeStatus={onFileChange}
-      InputComponent={selectFileInput}
-      getUploadParams={fileParams}
-      getFilesFromEvent={getFilesFromEvent}
-      accept=".jpg,.jpeg,.png,.pdf,.doc"
-      maxFiles={5}
-      inputContent="Trage sau click pentru a selecta fisiere"
-      styles={{
-        dropzone: { width: 900, height: 300, overflow: "hidden" },
-      }}
-    />
-  );
+    const selectFileInput = ({ accept, onFiles, files, getFilesFromEvent }) => {
+        const textMsg =
+            files.length > 0 ? "Mai selectează un fișier" : "Selectează fișierele din calculatorul tău";
+
+        return ( <
+            label className = "btn btn-danger mt-4 btn_upload" > { textMsg } <
+            input style = {
+                { display: "none" }
+            }
+            type = "file"
+            accept = { accept }
+            multiple onChange = {
+                (e) => {
+                    getFilesFromEvent(e).then((chosenFiles) => {
+                        onFiles(chosenFiles);
+                    });
+                }
+            }
+            /> < /
+            label >
+        );
+    };
+
+    return ( <
+        Dropzone onSubmit = { handleSubmit }
+        onChangeStatus = { onFileChange }
+        InputComponent = { selectFileInput }
+        getUploadParams = { fileParams }
+        getFilesFromEvent = { getFilesFromEvent }
+        accept = ".jpg,.jpeg,.png,.pdf,.doc"
+        maxFiles = { 5 }
+        inputContent = "Trage sau click pentru a selecta fisiere"
+        styles = {
+            {
+                dropzone: {},
+            }
+        }
+        />
+    );
 };
 
 export default FileUpload;
--- frontend/src/digitizare/Step1.js
+++ frontend/src/digitizare/Step1.js
@@ -4,37 +4,47 @@ import React, { Component, useRef } from "react";
 import FileUpload from "../components/FileUpload";
 
 export default class Step1 extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {}
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {}
 
-  componentWillUnmount() {}
+    componentWillUnmount() {}
 
-  // not required as this component has no forms or user entry
-  // isValidated() {}
+    // not required as this component has no forms or user entry
+    // isValidated() {}
 
-  render() {    
-    return (
-      <div className="step step1">
-        <div className="row">
-          <form id="Form" className="form-horizontal">
-            <div className="form-group">
-              <label className="col-md-12 control-label">
-                <h1>Pasul 1: Incarcă imagini sau fișiere PDF</h1>
-                <h3 className="info">
-                  Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf.
-                </h3>
-              </label>
-              <FileUpload getStore={() => this.props.getStore()}
-            updateStore={(u) => {
-              this.props.updateStore(u);
-            }}/>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = "step step1" >
+            <
+            div className = "row" >
+            <
+            form id = "Form"
+            className = "form-horizontal" >
+            <
+            div className = "form-group" >
+            <
+            label className = "col-md-12 control-label" >
+            <
+            h1 > Pasul 1: Incarcă imagini sau fișiere PDF < /h1> <
+            h3 className = "info" >
+            Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf. <
+            /h3> <
+            /label> <
+            FileUpload getStore = {
+                () => this.props.getStore() }
+            updateStore = {
+                (u) => {
+                    this.props.updateStore(u);
+                }
+            }
+            /> <
+            /div> <
+            /form> <
+            /div> <
+            /div>
+        );
+    }
 }
