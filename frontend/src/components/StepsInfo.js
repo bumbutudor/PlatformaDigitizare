d57205d40a9f4f08aa13207f6c3330f4c7a1547e
bumbutudor <bumbutudor10@gmail.com>
2022-02-24 22:29:31 +0200

    Add Info button for each step

---
+++ frontend/src/components/StepsInfo.js
@@ -0,0 +1,62 @@
+
+export default {
+    /**
+    * Pasul 1: Upload
+    */
+    step1Info: {
+        title: "Informații referitoare la pasul 1",
+        body: ''
+    },
+
+    /**
+    * Pasul 2: Preprocesare
+    */
+    step2Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf."
+    },
+
+    /**
+    * Pasul 3: OCR
+    */
+    step3Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf."
+    },
+
+    /**
+    * Pasul 4: Verificare OCR
+    */
+    step4Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Pregatirea si incarcarea <strong>documentului scanat din calculatorul utilizatorului. Se acceptă următoarele formate ale documentelor: png, jpg, tiff și pdf. Pot fi incarcate mai multe documente intr-un singur ciclu de digitizare. Calitatea imaginilor incarcate se pune in proportionalitate directa cu rezultele obtinute. O imagine unde la zoom maxim nu prea se vede bine continutul va genera un rezultat nesatisfacator. Un singur fisier incarcat nu va trece limita de 100MB. Toate fisierele incarcate la un singur ciclu de digitizare nu vor trece limita de 700MB. Este posibilitatea de a sterge unele documente care au fost intamplator selectate in acest pas."
+    },
+
+    /**
+    * Pasul 5: Transliterare
+    */
+    step5Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf."
+    },
+
+    /**
+    * Pasul 6: Verificare Transliterare
+    */
+    step6Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf."
+    },
+
+    /**
+    * Pasul 7: Salvarea rezultatelor
+    */
+    step6Info: {
+        title: "Pasul 1: Incarcă imagini sau fișiere PDF",
+        body: "Fișierele pot fi de tipul: .jpg, .jpeg, .png, .tif, .pdf."
+    },
+
+
+
+
+};
