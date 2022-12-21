19ea9a91cf535f70bf075414fabc57b9896377b0
bumbutudor <bumbutudor10@gmail.com>
2022-12-21 16:02:42 +0200

    add CORS_ALLOW_ALL_ORIGINS

--- backend/settings/settings.py
+++ backend/settings/settings.py
@@ -54,6 +54,15 @@ MIDDLEWARE = [
 
 
 CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    'http://localhost:8080',
+    'https://digitizare.math.md',
+    'https://ngrok.io',
+    'https://a1ef-81-180-76-251.eu.ngrok.io',
+]
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
